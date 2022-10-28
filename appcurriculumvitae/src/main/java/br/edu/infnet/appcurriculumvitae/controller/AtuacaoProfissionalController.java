4d90cc51c9e33c3c7d9f8ebe41a8c283f1f0aaed
Hamilton Valério Filho <h.valerio.filho@avanade.com>
2022-10-28 14:02:34 -0300

    ProjetoController

--- appcurriculumvitae/src/main/java/br/edu/infnet/appcurriculumvitae/controller/AtuacaoProfissionalController.java
+++ appcurriculumvitae/src/main/java/br/edu/infnet/appcurriculumvitae/controller/AtuacaoProfissionalController.java
@@ -1,13 +1,14 @@
 package br.edu.infnet.appcurriculumvitae.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AtuacaoProfissionalController {
 	
 	@GetMapping(value = "/atuacaoprofissional/lista")
-	public String telaLista() {
+	public String telaLista(Model model) {
 		return "atuacaoprofissional/lista";
 	}
 	
--- appcurriculumvitae/src/main/java/br/edu/infnet/appcurriculumvitae/controller/ProjetoController.java
+++ appcurriculumvitae/src/main/java/br/edu/infnet/appcurriculumvitae/controller/ProjetoController.java
@@ -1,14 +1,32 @@
 package br.edu.infnet.appcurriculumvitae.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import br.edu.infnet.appcurriculumvitae.model.domain.Projeto;
 
 @Controller
 public class ProjetoController {
 	
 	@GetMapping(value = "/projeto/lista")
-	public String telaLista() {
+	public String telaLista(Model model) {
 		return "projeto/lista";
+	}
+	
+	@PostMapping(value = "/projeto/salvar")
+	public RedirectView inserirProjeto(@ModelAttribute("projeto") Projeto projeto) {
+		final RedirectView redirectView = new RedirectView("/projeto/salvar", true);
+		return redirectView;
+	}
+	
+	@GetMapping(value = "/projeto/buscar/{titulo}")
+	public Projeto buscarProjeto(@PathVariable("titulo") String titulo) {
+		return null;
 	}
 	
 }
